330e1f4d38611ef434de7d5c1dd2ffcef65e1365
김지우 <kimjiwoo@gimjiuui-MacBookPro.local>
2023-02-09 16:46:31 +0900

    day03-review

--- class/03-01-rest-api-with-express/index.js
+++ class/03-01-rest-api-with-express/index.js
@@ -1,12 +1,12 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
 // GET 요청이 들어왔을 때
-app.get('/', (req, res) => {
-  res.send('Hello World!') // 응답 보내기
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!"); // 응답 보내기
+});
 
 app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`) 
-}) // 3000번 포트에서 24시간 실행
+  console.log(`Example app listening on port ${3000}`);
+}); // 3000번 포트에서 24시간 실행
